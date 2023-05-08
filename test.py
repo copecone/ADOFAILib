b892eaca31a693efd44ad4ae0e84ecd84b961d10
컨트롤D <controld3785@gmail.com>
2023-05-08 17:52:30 +0900

    Remove Some Test Codes

--- test.py
+++ test.py
@@ -7,30 +7,6 @@ def getrand(): return (random.random() - 0.5) * 2
 adofaiParser = ADOFAIParser("test.adofai")
 adofaiLevel = adofaiParser.getLevel()
 
-# Generate Level
-adofaiLevel.disable("TrackDisappearAnimation")
-adofaiLevel.disable("TrackAppearAnimation")
-
-for action in adofaiLevel.data["actions"]:
-    if action["eventType"] == "AnimateTrack":
-        action["trackDisappearAnimation"] = "None"
-        action["trackAnimation"] = "None"
-
-tileInit = 120
-tileAppearAnimation = -2
-tileDisappearAnimation = 8
-for i in range(adofaiLevel.length):
-    adofaiLevel.moveTrack(i + 1, range = -1, duration = 4 * adofaiLevel[i + 1]["bpmScale"], ease = Easing("InQuart"), opacity = 1000, rotation = -20, position = [getrand() * 1.5, -3], scale = [80, 80], angleOffset = 180 * tileDisappearAnimation * adofaiLevel[i + 1]["bpmScale"])
-
-    adofaiLevel.moveTrack(i + 1, range = 0, duration = 0, ease = Easing("Linear"), opacity = 0, rotation = 15, position = [0.2, 0.6], angleOffset = -180 * tileInit * adofaiLevel[i + 1]["bpmScale"])
-    adofaiLevel.moveTrack(i + 1, range = 0, duration = 4 * adofaiLevel[i + 1]["bpmScale"], ease = Easing("OutQuart"), opacity = 100, rotation = 0, position = [0, 0], angleOffset = -180 * tileAppearAnimation * adofaiLevel[i + 1]["bpmScale"])
-
-adofaiParser.save(adofaiLevel, "test2.adofai")
-
-# Beats Ahead Calculation
-adofaiParser.reload()
-adofaiLevel = adofaiParser.getLevel()
-
 adofaiLevel.disable("TrackDisappearAnimation")
 adofaiLevel.disable("TrackAppearAnimation")
 
@@ -62,4 +38,4 @@ for i in range(adofaiLevel.length):
     adofaiLevel.moveTrack(i + 1, range = 0, duration = 0, ease = Easing("Linear"), opacity = 0, rotation = 15, position = [0.2, 0.6], angleOffset = -180 * tileInit)
     adofaiLevel.moveTrack(i + 1, range = 0, duration = 4 * adofaiLevel[i + 1]["bpmScale"], ease = Easing("OutQuart"), opacity = 100, rotation = 0, position = [0, 0], angleOffset = -180 * tileAppearAnimation * adofaiLevel[i + 1]["bpmScale"])
 
-adofaiParser.save(adofaiLevel, "test3.adofai")
+adofaiParser.save(adofaiLevel, "test2.adofai")
